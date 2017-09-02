6715a43a5e122c5697a41ac353a0c4d99ae2ea71
murali mutyala <mutyala_murali@outlook.com>
2017-09-02 21:47:37 +0530

    Adds public/index file

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * Copyright (C) 2017 Murali Mutyala
+ * All rights are reserved under MIT license
+ * ----------------------------------------------------------------------
+ * Created on 09/02/2017 by muralimutyala
+ * ----------------------------------------------------------------------
+ */
+require '../vendor/autoload.php';
+
+
