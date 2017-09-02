b7f969dbd3303b971ab10dad3a54d2ecb48833c8
murali mutyala <mutyala_murali@outlook.com>
2017-09-02 21:49:07 +0530

    Adds Database constants file

---
+++ Constants.php
@@ -0,0 +1,41 @@
+<?php
+
+/* 
+ * Copyright (C) 2017 Murali Mutyala
+ * All rights are reserved under MIT license
+ * ----------------------------------------------------------------------
+ * Created on 09/02/2017 by muralimutyala
+ * ----------------------------------------------------------------------
+ */
+
+//Databse Constatns
+
+/**
+ * Constant for DATABASE SERVER
+ * @var string 
+ */
+defined('DB_SERVER') ? NULL : define('DB_SERVER', 'localhost');
+
+/**
+ * Constant for DATABASE PORT
+ * @var integer 
+ */
+defined('DB_PORT') ? NULL : define('DB_PORT', 8889);
+
+/**
+ * Constant for DATABASE USER
+ * @var string 
+ */
+defined('DB_USER') ? NULL : define('DB_USER', 'root');
+
+/**
+ * Constant for DATABASE PASSWORD
+ * @var string 
+ */
+defined('DB_PASSWORD') ? NULL : define('DB_PASSWORD', 'root');
+
+/**
+ * Constant for DATABASE NAME
+ * @var string 
+ */
+defined('DB_NAME') ? NULL : define('DB_NAME', 'sleek');
