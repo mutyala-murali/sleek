bf68d94232b13a2a19783423e60af381a73a9f85
murali mutyala <mutyala_murali@outlook.com>
2017-09-04 02:59:00 +0530

    UPDATE index.php with Routing

--- public/index.php
+++ public/index.php
@@ -5,8 +5,12 @@
  * All rights are reserved under MIT license
  * ----------------------------------------------------------------------
  * Created on 09/02/2017 by muralimutyala
+ * Updated on 09/04/2017 by muralimutyala
  * ----------------------------------------------------------------------
  */
 require '../vendor/autoload.php';
 
+use App\Core\Route;
 
+$route = new Route();
+$route->run();
